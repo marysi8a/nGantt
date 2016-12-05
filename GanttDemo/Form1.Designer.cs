43f12a380e20d7a55a7648d6c6e7426ab5e05af2
Marysia Gruszka <200326@student.pwr.wroc.pl>
2016-12-06 00:01:52 +0100

    first

---
+++ GanttDemo/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿using System.Windows.Forms;
+
+namespace GanttDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.AddBlock_Button = new System.Windows.Forms.Button();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.ganttControl1 = new nGantt.GanttControl();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.AddRowButton = new System.Windows.Forms.Button();
+            this.RowChosen = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonNext);
+            this.panel1.Controls.Add(this.buttonPrevious);
+            this.panel1.Controls.Add(this.dateTimePicker);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1108, 57);
+            this.panel1.TabIndex = 1;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(399, 12);
+            this.buttonNext.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(100, 28);
+            this.buttonNext.TabIndex = 2;
+            this.buttonNext.Text = ">";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Location = new System.Drawing.Point(16, 12);
+            this.buttonPrevious.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(100, 28);
+            this.buttonPrevious.TabIndex = 1;
+            this.buttonPrevious.Text = "<";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "HH:mm:ss d.MM.yyyy";
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(124, 15);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker.TabIndex = 0;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // AddBlock_Button
+            // 
+            this.AddBlock_Button.Location = new System.Drawing.Point(150, 554);
+            this.AddBlock_Button.Name = "AddBlock_Button";
+            this.AddBlock_Button.Size = new System.Drawing.Size(84, 22);
+            this.AddBlock_Button.TabIndex = 2;
+            this.AddBlock_Button.Text = "Add Block";
+            this.AddBlock_Button.UseVisualStyleBackColor = true;
+            this.AddBlock_Button.Click += new System.EventHandler(this.AddBlock_Clik);
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.elementHost1.Location = new System.Drawing.Point(0, 57);
+            this.elementHost1.Margin = new System.Windows.Forms.Padding(4);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(1108, 615);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.ganttControl1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "HH:mm:ss d.MM.yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(13, 525);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "HH:mm:ss d.MM.yyyy";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(299, 525);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // AddRowButton
+            // 
+            this.AddRowButton.Location = new System.Drawing.Point(588, 554);
+            this.AddRowButton.Name = "AddRowButton";
+            this.AddRowButton.Size = new System.Drawing.Size(84, 22);
+            this.AddRowButton.TabIndex = 5;
+            this.AddRowButton.Text = "Add Row";
+            this.AddRowButton.UseVisualStyleBackColor = true;
+            this.AddRowButton.Click += new System.EventHandler(this.AddRow_Click);
+            // 
+            // RowChosen
+            // 
+            this.RowChosen.Location = new System.Drawing.Point(16, 554);
+            this.RowChosen.Name = "RowChosen";
+            this.RowChosen.Size = new System.Drawing.Size(27, 22);
+            this.RowChosen.TabIndex = 6;
+            this.RowChosen.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 672);
+            this.Controls.Add(this.RowChosen);
+            this.Controls.Add(this.AddRowButton);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.AddBlock_Button);
+            this.Controls.Add(this.elementHost1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "GanttDemo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private nGantt.GanttControl ganttControl1;
+        private System.Windows.Forms.Button AddBlock_Button;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private Button AddRowButton;
+        private TextBox RowChosen;
+    }
+}
+
---
+++ GanttDemo/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using nGantt.GanttChart;
+using nGantt.PeriodSplitter;
+namespace GanttDemo
+{
+    public struct zadania
+    {
+        public int start;
+        public int end;
+        public int nr;
+
+    };
+    public partial class Form1 : Form
+    {
+        private int GantLenght { get; set; }
+        private ObservableCollection<ContextMenuItem> ganttTaskContextMenuItems = new ObservableCollection<ContextMenuItem>();
+        private ObservableCollection<SelectionContextMenuItem> selectionContextMenuItems = new ObservableCollection<SelectionContextMenuItem>();
+        List<GanttRow> RowList = new List<GanttRow>();
+        List<GanttRowGroup> RowGroupList = new List<GanttRowGroup>();
+        List<zadania> Kolejka = new List<zadania>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GantLenght = 2;
+            dateTimePicker.Value = DateTime.Now;
+            DateTime minDate = dateTimePicker.Value.Date;
+            DateTime maxDate = minDate.AddDays(GantLenght);
+
+            // Set selection -mode
+            ganttControl1.TaskSelectionMode = nGantt.GanttControl.SelectionMode.Single;
+            // Enable GanttTasks to be selected
+            ganttControl1.AllowUserSelection = true;
+
+            // listen to the GanttRowAreaSelected event
+            ganttControl1.GanttRowAreaSelected += new EventHandler<PeriodEventArgs>(ganttControl1_GanttRowAreaSelected);
+            
+            // define ganttTask context menu and action when each item is clicked
+            ganttTaskContextMenuItems.Add(new ContextMenuItem(ViewClicked, "View..."));
+            ganttTaskContextMenuItems.Add(new ContextMenuItem(EditClicked, "Edit..."));
+            ganttTaskContextMenuItems.Add(new ContextMenuItem(DeleteClicked, "Delete..."));
+            ganttControl1.GanttTaskContextMenuItems = ganttTaskContextMenuItems;
+
+            // define selection context menu and action when each item is clicked
+            selectionContextMenuItems.Add(new SelectionContextMenuItem(NewClicked, "New..."));
+            ganttControl1.SelectionContextMenuItems = selectionContextMenuItems;
+
+        }
+
+        private void NewClicked(Period selectionPeriod)
+        {
+            MessageBox.Show("New clicked for task " + selectionPeriod.Start.ToString() + " -> " + selectionPeriod.End.ToString());
+        }
+
+        private void ViewClicked(GanttTask ganttTask)
+        {
+            MessageBox.Show("New clicked for task " + ganttTask.Name);
+        }
+
+        private void EditClicked(GanttTask ganttTask)
+        {
+            MessageBox.Show("Edit clicked for task " + ganttTask.Name);
+        }
+
+        private void DeleteClicked(GanttTask ganttTask)
+        {
+            MessageBox.Show("Delete clicked for task " + ganttTask.Name);
+        }
+
+        void ganttControl1_GanttRowAreaSelected(object sender, PeriodEventArgs e)
+        {
+            MessageBox.Show(e.SelectionStart.ToString("0:HH:mm d.MM.yyyy") + " -> " + e.SelectionEnd.ToString("0:HH:mm d.MM.yyyy"));
+        }
+
+        private System.Windows.Media.Brush DetermineBackground(TimeLineItem timeLineItem)
+        {
+            if (timeLineItem.End.Date.DayOfWeek == DayOfWeek.Saturday || timeLineItem.End.Date.DayOfWeek == DayOfWeek.Sunday)
+                return new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.LightBlue);
+            else
+                return new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Transparent);
+        }
+
+        private void CreateData(DateTime minDate, DateTime maxDate)
+        {
+            // Set max and min dates
+            ganttControl1.Initialize(minDate, maxDate);
+            //header displaying
+            // Create timelines and define how they should be presented
+            //ganttControl1.CreateTimeLine(new PeriodYearSplitter(minDate, maxDate), FormatYear);
+            //ganttControl1.CreateTimeLine(new PeriodMonthSplitter(minDate, maxDate), FormatMonth);
+            var gridLineTimeLine = ganttControl1.CreateTimeLine(new PeriodYearSplitter(minDate, maxDate), FormatYear);
+            ganttControl1.CreateTimeLine(new PeriodDaySplitter(minDate, maxDate), FormatDayName);
+
+            // Set the timeline to atatch gridlines to
+            ganttControl1.SetGridLinesTimeline(gridLineTimeLine, DetermineBackground);
+
+            // Create and data
+            RowGroupList.Add(ganttControl1.CreateGanttRowGroup("HeaderdGanttRowGroup"));
+            RowList.Add(ganttControl1.CreateGanttRow(RowGroupList[0], "GanttRow 1")); 
+            ganttControl1.AddGanttTask(RowList[0], new GanttTask() { Start = new DateTime(2016,12,05,13,24,00), End = new DateTime(2016, 12, 05, 13, 44, 00), Name = "GanttRow 1:GanttTask 1", TaskProgressVisibility = System.Windows.Visibility.Hidden });
+            ganttControl1.AddGanttTask(RowList[0], new GanttTask() { Start = DateTime.Now.AddHours(3), End = DateTime.Now.AddHours(5), Name = "GanttRow 1:GanttTask 2" });
+            ganttControl1.AddGanttTask(RowList[0], new GanttTask() { Start = DateTime.Now.AddHours(4), End = DateTime.Now.AddHours(8), Name = "GanttRow 1:GanttTask 3" });
+
+            RowGroupList.Add(new ExpandableGanttRowGroup());
+            RowGroupList[1] = ganttControl1.CreateGanttRowGroup("ExpandableGanttRowGroup", true);
+
+            RowList.Add(ganttControl1.CreateGanttRow(RowGroupList[1], "GanttRow 2"));
+            RowList.Add(ganttControl1.CreateGanttRow(RowGroupList[1], "GanttRow 3"));
+
+            ganttControl1.AddGanttTask(RowList[1], new GanttTask() { Start = DateTime.Now.AddHours(1), End = DateTime.Now.AddHours(4), Name = "GanttRow 2:GanttTask 1" });
+            ganttControl1.AddGanttTask(RowList[1], new GanttTask() { Start = DateTime.Now.AddHours(2), End = DateTime.Now.AddHours(3), Name = "GanttRow 2:GanttTask 2" });
+            ganttControl1.AddGanttTask(RowList[1], new GanttTask() { Start = DateTime.Now.AddHours(3), End = DateTime.Now.AddHours(4), Name = "GanttRow 2:GanttTask 3", PercentageCompleted = 0.375 });
+            ganttControl1.AddGanttTask(RowList[2], new GanttTask() { Start = DateTime.Now.AddHours(4), End = DateTime.Now.AddHours(5), Name = "GanttRow 3:GanttTask 1", PercentageCompleted = 0.5 });
+
+            RowGroupList.Add(ganttControl1.CreateGanttRowGroup());
+            RowList.Add(ganttControl1.CreateGanttRow(RowGroupList[2], "GanttRow 4"));
+            ganttControl1.AddGanttTask(RowList[3], new GanttTask() { Start = DateTime.Now.AddHours(7), End = DateTime.Now.AddHours(8), Name = "GanttRow 4:GanttTask 1", PercentageCompleted = 1 });
+            ganttControl1.AddGanttTask(RowList[3], new GanttTask() { Start = DateTime.Now.AddHours(4), End = DateTime.Now.AddHours(7), Name = "GanttRow 4:GanttTask 2" });
+            ReadFile();
+            AddRow();
+            foreach (var s in Kolejka)
+            {
+                AddBlock(s.start, s.end, 5, s.nr.ToString() + ". Task");
+            }
+        }
+    
+        private string FormatYear(Period period)
+        {
+            return (
+                period.Start.DayOfWeek.ToString() + ", " +
+                period.Start.Day.ToString() + "." +
+                period.Start.Month.ToString() + "." +
+                period.Start.Year.ToString());
+        }
+
+        private string FormatMonth(Period period)
+        {
+            return period.Start.Month.ToString();
+        }
+
+        private string FormatDay(Period period)
+        {
+            return period.Start.Day.ToString();
+        }
+
+        private string FormatDayName(Period period)
+        {
+            return period.Start.DayOfWeek.ToString();
+        }
+
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            dateTimePicker.Value = ganttControl1.GanttData.MinDate.AddDays(-GantLenght);
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            dateTimePicker.Value = ganttControl1.GanttData.MaxDate;
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime minDate = dateTimePicker.Value.Date;
+            DateTime maxDate = minDate.AddDays(GantLenght);
+            ////DateTime minDate = new DateTime(2016, 12, 03, 13, 24, 00);
+            ////DateTime maxDate = new DateTime(2016, 12, 08, 13, 24, 00);
+            ganttControl1.ClearGantt();
+            RowList.Clear();
+            RowGroupList.Clear();
+            CreateData(minDate, maxDate);
+        }
+        
+
+        private void AddRow_Click(object sender, EventArgs e)
+        {
+            RowList.Add(ganttControl1.CreateGanttRow(RowGroupList[1], "GanttRow 3"));
+        }
+        private void AddRow()
+        {
+            RowList.Add(ganttControl1.CreateGanttRow(RowGroupList[1], "New"));
+        }
+
+        private void AddBlock_Clik(object sender, EventArgs e)
+        {
+            ganttControl1.AddGanttTask(RowList[Int32.Parse(RowChosen.Text)-1], new GanttTask() { Start = dateTimePicker1.Value, End = dateTimePicker2.Value, Name = "GanttRow 2:GanttTask 1" });
+        }
+        private void AddBlock(int start, int end, int machinenr, string name)
+        {
+            // ganttControl1.AddGanttTask(RowList[machinenr - 1], new GanttTask() { Start = DateTime.Now.AddHours(start), End = DateTime.Now.AddHours(end), Name = "GanttRow 2:GanttTask 1" });
+             ganttControl1.AddGanttTask(RowList[machinenr - 1], new GanttTask() { Start = DateTime.Now.AddMinutes(start), End = DateTime.Now.AddMinutes(end), Name = name });
+        }
+
+        void ReadFile()
+        {
+            string cmdOutput;
+
+            // Schrage & Schrage z podziałem (tablice dynamiczne)
+            Process proc = new Process();
+            proc.StartInfo.FileName = "SPD.exe";
+            proc.StartInfo.Arguments = "inJ.txt";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.Start();
+
+            cmdOutput = proc.StandardOutput.ReadToEnd();
+
+            proc.WaitForExit();
+            proc.Close();
+            cmdOutput = cmdOutput.Replace("\r\n", " ");
+            string[] s = cmdOutput.Split();
+            int n = Int32.Parse(s[0]);
+            int a, b,c;
+            int j = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                 a = int.Parse(s[j]);
+                 b = int.Parse(s[j+1]);
+                 c = int.Parse(s[j+2]);
+                zadania tmp=new zadania();
+                tmp.nr = a;
+                tmp.start = b;
+                tmp.end = c;
+                Kolejka.Add(tmp);
+                j += 3;
+            }
+        }
+    }
+}
